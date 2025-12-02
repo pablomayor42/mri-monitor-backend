06d9a2de197eb899450acc7c1ac8faf5e239a670
pablomayor42 <pablomayortomillo@gmail.com>
2025-12-02 19:15:02 +0100

    mejora de vista

--- mri_monitor/settings.py
+++ mri_monitor/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dev-secret-key')
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
